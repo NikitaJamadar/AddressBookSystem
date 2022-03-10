160cdd690b5a3a78ed396d3bb958e3ed731e8fe5
NikitaJamadar <nikhitajamadar1998@gmail.com>
2022-03-10 09:08:12 +0530

    Add contacts in Address Book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,6 +7,11 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book system");
+
+            AddressBook.EnterDetails();
+            AddressBook obj = new AddressBook();
+            obj.ViewTheDetails();
+            Console.ReadLine();
         }
     }
 }
