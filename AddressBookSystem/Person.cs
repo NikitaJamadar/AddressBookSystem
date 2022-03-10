274cd38a8ce22c45e6c86cd0d3f3868430f0ef94
NikitaJamadar <nikhitajamadar1998@gmail.com>
2022-03-10 09:01:04 +0530

    Create Address book

--- AddressBookSystem/Person.cs
+++ AddressBookSystem/Person.cs
@@ -6,5 +6,14 @@ namespace AddressBookSystem
 {
     class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+
     }
 }
